cd851d1741d90bdba295ec0282ad0d50055269f3
yusufolola <yusufoyesanmi@yahoo.com>
2024-03-07 07:23:54 +0100

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -8,16 +8,15 @@ from datetime import datetime
 class BaseModel:
     """this defines all common attributes and methods for other classes"""
 
-    def __init__ (self, id ,created_at, updated_at):
-        """ instantiating the attributes"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+    id = str(uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
 
     def __str__(self):
         """prints human readable description"""
 
-        print([self.__class__.__name__] (self.id) self.__dict__)
+        return f"({[self.__class__.__name__]} {(self.id)} {self.__dict__})"
 
         
     def save(self):
@@ -29,8 +28,8 @@ class BaseModel:
         """converts the object to a dictionary"""
 
         obj_dict = self.__dict__.copy()
-        obj_dict.__class__ = self.__class__.__name__
-        obj_dict.created_at = self.created_at.isoformat()
-        obj_dict.updated_at = self.created_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict['created_a'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.created_at.isoformat()
         return obj_dict
 
