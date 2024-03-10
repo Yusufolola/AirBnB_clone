0d6d443254ca93cce0fce91122e633adeeecc28a
wahab <wabsid01@gmail.com>
2024-03-10 10:28:45 -0700

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,72 +1,55 @@
 #!/usr/bin/python3
-"""class tthat serializes instances to a JSON file and bach to instance"""
-import JSON
+"""
+Module serialize and deserialize JSON file and storage
+"""
+
+import json
 import models
 from models.base_model import BaseModel
 from models.user import User
 import os
 
 
-class FileStorage:
-    """this serializes instances to a Json file and deserializes JSON file to instances"""
+class FileStorage():
+    """
+    A class that serializes instances to a
+    JSON file and deserializes JSON file to instances
+    """
 
-<<<<<<< HEAD
     __file_path = "file.json"
-=======
-    __file_path = 'file.json'
->>>>>>> 67846aa7c7c77f8e3ebeeaacddea78f0b72d8e5b
     __objects = {}
 
     def all(self):
         """
-        Method that returns the dictionary __object
+        A method returns the dictionary __objects
         """
-        returns (self.__objects)
+        return self.__objects
 
     def new(self, obj):
-<<<<<<< HEAD
-       # setattr
         """
-        Custom method sets in __objects 
+        A method sets in __objects the obj with key <obj class name>.id
         """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
         """
-        The  method that serializes __objects to the JSON file (path: __file_path)
+        A method serializes __objects to the JSON file (path: __file_path)
         """
         dict_save = {}
-        with open(self.__file_path, "w+", encoding="UTF-8") as myFile:
+        with open(self.__file_path, "w+", encoding="UTF-8") as f:
             for key, value in self.__objects.items():
                 dict_save[key] = value.to_dict()
-            json.dump(dict_save, myFile)
+            json.dump(dict_save, f)
 
     def reload(self):
         """
         A method deserializes the JSON file to __objects
         """
         if os.path.isfile(FileStorage.__file_path):
-            with open(self.__file_path, mode="r+", encoding="UTF-8") as myFile:
-                data = json.load(myFile)
+            with open(self.__file_path, mode="r+", encoding="UTF-8") as f:
+                data = json.load(f)
             for key, value in data.items():  # **kwargs
                 class_obj = value.get('__class__')
                 if class_obj in models.dict_class:
                     self.__objects[key] = models.dict_class[class_obj](**value)
-=======
-        for key, value in obj:
-            setattr(self ,key ,value)
-
-    def save(self):
-        with open(__file_path, 'w') as f:
-            json.dump(self.__objects, f)
-
-    def reload(self):
-        if __file_path:
-            with open(__file_path, 'r') as f:
-                return json.load(f)
-        else:
-            pass
->>>>>>> 67846aa7c7c77f8e3ebeeaacddea78f0b72d8e5b
-
-
